bd72cac3a187af67f1d94f5f957e9bd9acc505da
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-04-18 22:43:14 +0300

    handling PATCH and DELETE requests

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json())
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -17,8 +18,28 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const id = req.params.id * 1;
+
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+    const tour = tours.find(tour => tour.id === id)
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+})
+
 app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body)
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body)
     tours.push(newTour);
@@ -30,7 +51,36 @@ app.post('/api/v1/tours', (req, res) => {
             }
         })
     })
+})
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: "<Updated tour here...>"
+        }
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+
+    res.status(204).json({
+        status: "success",
+        data: null
+    })
 })
 
 const PORT = 3000;
