e8e7b3354af572cc31a2bf6b38df671565b64599
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-04-18 22:49:15 +0300

    code refractoring

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -16,10 +16,9 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     })
-})
+}
 
-
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
 
     const id = req.params.id * 1;
 
@@ -37,9 +36,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
             tour
         }
     })
-})
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body)
     tours.push(newTour);
@@ -51,9 +50,9 @@ app.post('/api/v1/tours', (req, res) => {
             }
         })
     })
-})
+}
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
     if (req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: "fail",
@@ -67,9 +66,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             tour: "<Updated tour here...>"
         }
     })
-})
+}
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
     if (req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: "fail",
@@ -81,7 +80,13 @@ app.delete('/api/v1/tours/:id', (req, res) => {
         status: "success",
         data: null
     })
-})
+}
+
+app.get('/api/v1/tours', getAllTours)
+app.get('/api/v1/tours/:id', getTour)
+app.post('/api/v1/tours', createTour)
+app.patch('/api/v1/tours/:id', updateTour)
+app.delete('/api/v1/tours/:id', deleteTour)
 
 const PORT = 3000;
 app.listen(PORT, () => {
