1fcfeb948bb1b752baa0f6e0ab2a817725d77af0
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-04-18 22:09:00 +0300

    handling POST request

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 app.get('/api/v1/tours', (req, res) => {
@@ -14,6 +15,22 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body)
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
+    })
+
 })
 
 const PORT = 3000;
