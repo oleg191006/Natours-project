eca0d55ab1d3f8bdbcfb64e25129bd8674ce80dd
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-04-19 12:21:30 +0300

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 const app = require('./app')
+
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE;
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => { console.log('DB connection successfull!') })
 
 const PORT = 3000;
 app.listen(PORT, () => {
