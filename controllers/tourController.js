4e937300854c952c6f90f406f615bdd8286859f1
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-05-03 11:38:55 +0300

    add error controller and update tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,26 +1,33 @@
 
 const Tour = require('./../models/tourModel')
+const APIFeatures = require('./../utils/apifeatures');
 
 
 exports.getAllTours = async (req, res) => {
-
     try {
-        const tours = await Tour.find()
+        // EXECUTE QUERY
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const tours = await features.query;
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
             data: {
                 tours
             }
-        })
-    } catch (error) {
+        });
+    } catch (err) {
         res.status(404).json({
-            status: "fail",
-            message: error
-        })
+            status: 'fail',
+            message: err
+        });
     }
-
-}
+};
 
 exports.getTour = async (req, res) => {
     try {
